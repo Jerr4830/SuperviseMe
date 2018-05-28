dbca99f502f656d7e03bcac656ace07993ce9d92
Jerry RA <jerry.ra1408@gmai.com>
2018-05-28 13:58:51 -0400

    upload video version 1

--- users/video_gallery.php
+++ users/video_gallery.php
@@ -3,7 +3,7 @@ session_start();
 ?>
 <html>
 <head>
-<title>Upload Video</title>
+<title>Video Gallery</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
